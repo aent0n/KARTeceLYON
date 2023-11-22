6933fad172f4e7cffbaf63ce08a209cf6a786398
Anton Adam <116871473+aent0n@users.noreply.github.com>
2023-11-22 02:56:50 +0100

    Update script.js

--- mobileLookupTool/script.js
+++ mobileLookupTool/script.js
@@ -10,31 +10,31 @@ function searchName() {
     }
 
     fetch(url)
-    .then(response => response.text())
-    .then(data => {
-        const lines = data.split("\n");
-        const formattedNames = lines.map(line => line.split(",")[0].toLowerCase().trim()); // Convert names to lowercase and remove extra spaces
+        .then(response => response.text())
+        .then(data => {
+            const lines = data.split("\n");
+            const formattedNames = lines.map(line => line.split(",")[0].toLowerCase().trim()); // Convert names to lowercase and remove extra spaces
 
-        // Check for direct match, excluding "BLANK"
-        const directMatchIndex = formattedNames.findIndex(name => name === enteredName && name !== "blank");
-        if (directMatchIndex !== -1) {
-            displayResult(lines[directMatchIndex]);
-            return;
-        }
+            // Check for direct match
+            const directMatchIndex = formattedNames.findIndex(name => name === enteredName);
+            if (directMatchIndex !== -1) {
+                displayResult(lines[directMatchIndex]);
+                return;
+            }
 
-        // Check for reversed order match
-        const [firstName, lastName] = enteredName.split(" ");
-        const reversedName = `${lastName} ${firstName}`;
-        const reversedMatchIndex = formattedNames.findIndex(name => name === reversedName);
-        if (reversedMatchIndex !== -1) {
-            displayResult(lines[reversedMatchIndex]);
-            return;
-        }
+            // Check for reversed order match
+            const [firstName, lastName] = enteredName.split(" ");
+            const reversedName = `${lastName} ${firstName}`;
+            const reversedMatchIndex = formattedNames.findIndex(name => name === reversedName);
+            if (reversedMatchIndex !== -1) {
+                displayResult(lines[reversedMatchIndex]);
+                return;
+            }
 
-        // Handle no match
-        displayNoResult();
-    })
-    .catch(error => console.log(error));
+            // Handle no match
+            displayNoResult();
+        })
+        .catch(error => console.log(error));
 }
 
 function displayResult(result) {
@@ -83,18 +83,89 @@ searchButton.addEventListener("click", function(event) {
     searchName();
 });
 
-const searchBar = document.getElementById("searchBar");
-searchBar.addEventListener("input", function() {
-    const resultDiv = document.getElementById("resultDiv");
-    if (resultDiv) {
-        resultDiv.remove();
+searchBar.addEventListener("input", async function(event) {
+    const enteredName = searchBar.value.toLowerCase().trim(); // Convert to lowercase and remove extra spaces
+    if (enteredName === "") {
+        clearDisplay();
+        return;
+    }
+    try {
+        const suggestedNames = await getSuggestedNames(enteredName); // Wait for the Promise to resolve
+        displaySuggestedNames(suggestedNames); // Call the displaySuggestedNames function with the retrieved suggested names
+    } catch (error) {
+        console.error(error);
+        // Handle the error, e.g., display an error message to the user
     }
 });
 
-const searchBar2 = document.getElementById("searchBar");
-searchBar2.addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-        event.preventDefault(); // Prevent the default form submission behavior
-        searchName();
+function clearDisplay() {
+    const suggestedNamesContainer = document.getElementById("suggestedNamesContainer");
+    suggestedNamesContainer.innerHTML = ""; // Clear suggested names
+
+    const resultDiv = document.getElementById("resultDiv");
+    if (resultDiv) {
+        resultDiv.remove(); // Clear result div
     }
-});
+}
+
+function getSuggestedNames(enteredName) {
+    // Fetch the data from the Google document
+    const url = "https://docs.google.com/document/d/1tZGAF2po4KXmNW0n6ZTDc1WiReEA3CQxdaklmBW4RHU/export?format=txt";
+
+    return fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            // Process the data to extract the names for suggesting based on the user input
+            const lines = data.split("\n");
+            const suggestedNames = lines.map(line => line.split(",")[0].trim()); // Extract names from the lines
+
+            return suggestedNames.filter(fullName => {
+                // Split the full name into words
+                const nameParts = fullName.split(" ");
+                // Check each part of the name for a match
+                return nameParts.some(namePart => hasThreeOrMoreSameCharacters(namePart, enteredName));
+            });
+        })
+        .catch(error => {
+            console.log(error);
+            return []; // Return an empty array in case of an error
+        });
+}
+function hasThreeOrMoreSameCharacters(name, enteredName) {
+    const nameLowerCase = name.toLowerCase();
+    const enteredNameLowerCase = enteredName.toLowerCase();
+
+    for (let i = 0; i <= enteredNameLowerCase.length - 3; i++) {
+        const subString = enteredNameLowerCase.substring(i, i + 3);
+        if (nameLowerCase.includes(subString)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function displaySuggestedNames(suggestedNames) {
+    const suggestedNamesContainer = document.getElementById("suggestedNamesContainer");
+    suggestedNamesContainer.innerHTML = ""; // Clear previous suggestions
+    suggestedNamesContainer.style.display = "flex"; // Display the buttons in a column
+    suggestedNamesContainer.style.flexDirection = "column";
+
+    if (Array.isArray(suggestedNames)) {
+        suggestedNames.forEach(name => {
+            const nameButton = document.createElement("button");
+            nameButton.innerText = name;
+            nameButton.classList.add("px-4", "ml-2", "py-2", "bg-[#A7A6AD]", "text-[#000000]", "rounded", "transition-transform", "hover:scale-105", "w-auto", "mt-2");
+            nameButton.addEventListener("click", function() {
+                document.getElementById("searchBar").value = name; // Auto-complete the text input
+                clearDisplay(); // Clear all suggested names
+                searchName(); // Validate the input request, e.g., trigger the searchName function
+            });
+            suggestedNamesContainer.appendChild(nameButton);
+        });
+    } else {
+        const errorDiv = document.createElement("div");
+        errorDiv.innerText = "Error: Suggested names data is not in the expected format";
+        suggestedNamesContainer.appendChild(errorDiv);
+    }
+}
